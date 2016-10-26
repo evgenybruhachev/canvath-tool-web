47c36fdc9947681310201c92bcd69a9b76a948e1
Alexey Korzun <akorzun@outlook.com>
2016-10-26 19:25:04 +0300

    drop-down select component

--- src/components/drop-down/index.js
+++ src/components/drop-down/index.js
@@ -1,14 +1,42 @@
 import React, { Component } from 'react';
+import ReactDOM from 'react-dom';
 
-function DropDown ({label, onSelect, onClick, style}){
-  return (
-    <button className='drop-down' label={label} style={style} onClick={onClick}>
-      <span className='label'>{label}</span>
-      <svg className='icon'>
-        <use xlinkHref={`#icon-list`} />
-      </svg>
-    </button>
-  )
+class DropDown extends Component{
+  _toggleList(){
+    this.refs._list.classList.toggle('active');
+  }
+
+  componentDidMount() {
+    this.__wrappedComponent = this.refs.dropDown;
+    document.addEventListener('click', this.handleClickOutside.bind(this), true);
+  }
+
+  componentWillUnmount() {
+    document.removeEventListener('click', this.handleClickOutside.bind(this), true);
+  }
+
+  handleClickOutside(e) {
+    const domNode = ReactDOM.findDOMNode(this);
+    if (!domNode.contains(e.target)) {
+
+      this.refs._list.classList.remove('active');
+    }
+  }
+
+  render() {
+    return (
+      <button className='drop-down' ref="dropDown" label={this.props.label} style={this.props.style}
+              onClick={this.props.onClick || this._toggleList.bind(this)}>
+        <span className='label'>{this.props.label}</span>
+        <svg className='icon'>
+          <use xlinkHref={`#icon-list`}/>
+        </svg>
+        <div className="list" ref="_list">
+          {this.props.children}
+        </div>
+      </button>
+    )
+  }
 };
 
 DropDown.propTypes = {
--- src/containers/Header/index.js
+++ src/containers/Header/index.js
@@ -28,8 +28,16 @@ class Header extends Component{
         <IconButton icon="poster" label='開く' onClick={this._openProductLoad.bind(this)}/>
         <IconButton icon="save" label='開く' />
         <DropDown label='Type' style={{width: 200+'px'}} onClick={this._openCategorySelect.bind(this)}/>
-        <DropDown label='Side' />
-        <DropDown label='Color' />
+        <DropDown label='Side'>
+          <div className="list-item">Side 1</div>
+          <div className="list-item">Side 2</div>
+          <div className="list-item">Side 3</div>
+        </DropDown>
+        <DropDown label='Color'>
+          <div className="list-item"><span className="color" style={{'backgroundColor': 'red'}}></span>Red</div>
+          <div className="list-item"><span className="color" style={{'backgroundColor': 'blue'}}></span>Blue</div>
+          <div className="list-item"><span className="color" style={{'backgroundColor': 'green'}}></span>Green</div>
+        </DropDown>
         <Price value='5000' />
       </div>
     )
