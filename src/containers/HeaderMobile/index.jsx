48bd380d43f0d5ad5fae9cd3c6e4b3ba49215fb1
Alexey Korzun <akorzun@outlook.com>
2016-12-12 16:19:53 +0300

    update url

--- src/containers/HeaderMobile/index.jsx
+++ src/containers/HeaderMobile/index.jsx
@@ -66,7 +66,7 @@ class HeaderMobile extends Component {
       saveProduct(colorSelected.id, sides).then((data) => {
         const form = document.createElement('form');
         form.setAttribute('method', 'post');
-        form.setAttribute('action', 'http://52.199.118.6/proc.php?run=appli2web');
+        form.setAttribute('action', 'http://nobori.decodeapps.io/proc.php?run=appli2web');
 
         for (const key in data) {
           const input = document.createElement('input');
