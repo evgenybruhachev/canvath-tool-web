609e37613c4eac2cb78bd468f3efc6329f11db96
Belyaev Andrey <belyaev_andrey_hr@mail.ru>
2017-01-24 10:53:31 +0300

    fix bug

--- webpack.config.js
+++ webpack.config.js
@@ -7,7 +7,6 @@ const autoprefixer = require('autoprefixer');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ExtractTextPlugin = require('extract-text-webpack-plugin');
 const CopyWebpackPlugin = require('copy-webpack-plugin');
-var $ = require('jquery');
 
 const config = {};
 
