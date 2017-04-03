57aaa4856bab5464e746c6fdbf9667351a2efb9a
evgenybruhachev <eugenbriukhachyov@192.168.1.41.ipl.orionnet.ru>
2017-04-03 17:54:52 +0700

    update hosts
    update hosts

--- src/constants/index.js
+++ src/constants/index.js
@@ -1,5 +1,8 @@
-// export const HOST = 'http://api.tprint.jp';
-// export const WEBHOST = 'http://tprint.jp';
+// export const HOST = 'http://api.up-t.jp';
+// export const WEBHOST = 'http://up-t.jp';
 
-export const HOST = 'http://api-up-t.decodeapps.io';
-export const WEBHOST = 'http://up-t.decodeapps.io';
+// export const HOST = 'http://api-up-t.decodeapps.io';
+// export const WEBHOST = 'http://up-t.decodeapps.io';
+
+export const HOST = 'http://api-beta.up-t.jp';
+export const WEBHOST = 'http://beta.up-t.jp';
